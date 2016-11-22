19dae8eaed93c594b3daac7baa9f6e8cdcc3e122
Abdelmunem Elbasiouny <abdelmunemelbasiouny@gmail.com>
2016-11-22 15:41:58 +0200

    test

--- app/src/main/java/com/abdelmun3m/prototype/User.java
+++ app/src/main/java/com/abdelmun3m/prototype/User.java
@@ -1,8 +1,27 @@
 package com.abdelmun3m.prototype;
 
+import com.google.firebase.database.DatabaseReference;
+
 /**
  * Created by Abdelmunem on 11/4/2016.
  */
 
 public class User {
+
+    public String name ;
+    public String sname;
+
+    public User(){
+
+    }
+    public User(String name , String s ){
+        this.name=name;
+        this.sname=s;
+    }
+
+    public void addNewuser(DatabaseReference r){
+
+        r.child("users").push().setValue(this);
+
+    }
 }

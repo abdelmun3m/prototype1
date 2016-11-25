aa85a57dd0541c35eff6518a8e2e7cf0bbeb43c2
omniaalaa <monyomnia68@gmail.com>
2016-11-26 00:19:34 +0200

    java class for profile activity

---
+++ app/src/main/java/com/abdelmun3m/prototype/ProfileScreenXMLUIDesign.java
@@ -0,0 +1,18 @@
+package com.abdelmun3m.prototype;
+
+/**
+ * Created by 3M on 24/11/2016.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class ProfileScreenXMLUIDesign extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.material_design_profile_screen_xml_ui_design);
+    }
+}

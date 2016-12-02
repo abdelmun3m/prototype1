ece46f6e77e5ce4d936d2f561b183e6fe4660ec4
Abdelmunem Elbasiouny <abdelmunemelbasiouny@gmail.com>
2016-12-02 20:05:33 +0200

    location class updates

--- app/src/main/java/com/abdelmun3m/prototype/Location.java
+++ app/src/main/java/com/abdelmun3m/prototype/Location.java
@@ -1,8 +1,43 @@
 package com.abdelmun3m.prototype;
 
-/**
- * Created by Abdelmunem on 11/4/2016.
- */
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class Location {
+
+    //--------------------------------------Properties----------------------------------------------
+    private String id;
+    //----------------------------------------------------------------------------------------------
+
+    //---------------------------------------Methods------------------------------------------------
+    //----------------------------------------------------------------------------------------------
+
+
+
+    //--------------------------------DataBase Interaction Methods ---------------------------------
+    public void addNewLocation(DatabaseReference R, Location l){
+        String id = R.child("location").push().getKey();
+        R.child("location").push().setValue(this);
+        this.id = id;
+    }
+    public ValueEventListener getLocation(){
+
+        ValueEventListener UserListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Location  l = dataSnapshot.getValue(Location.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("Error : ", ""+databaseError.toException());
+            }
+        };
+        return  UserListener;
+    }
+    //----------------------------------------------------------------------------------------------
 }

363b3eef3eb5191d1ea87bab26af49058ce53f1f
Abdelmunem Elbasiouny <abdelmunemelbasiouny@gmail.com>
2017-04-26 07:13:39 +0200

    Updated 7:13

--- app/src/main/java/com/abdelmun3m/prototype/DataBase.java
+++ app/src/main/java/com/abdelmun3m/prototype/DataBase.java
@@ -1,7 +1,16 @@
 package com.abdelmun3m.prototype;
 
+import android.app.Application;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -12,27 +21,20 @@ public class DataBase {
 
 
 
-    private FirebaseDatabase DBinsatnce;
-    private DatabaseReference DBreference;
-    private ValueEventListener ChangeListener;
+    private static final String TAG = "DataBaseClass";
+    public FirebaseDatabase DBinsatnce;
+    public DatabaseReference DBreference;
+    public ValueEventListener ValueListener;
+    public FirebaseAuth myAuth;
+    public FirebaseAuth.AuthStateListener myAuthListener;
+    public FirebaseUser User ;
+
 
 
     public  DataBase(){
         DBinsatnce = FirebaseDatabase.getInstance(); // get an instance from the database .
         DBreference = DBinsatnce.getReference() ; // getting the reference to the root of the data base
-    }
-
-    public DataBase(DatabaseReference R){
-        this.DBreference = R ;
-    }
-
-    public DatabaseReference  getDBreference(){
-        return DBreference;
-    }
-
-    //OverWrite on the data
-    public void Add(String Child , Object o){
-        DBreference.child(Child).push().setValue(o);
+        myAuth = FirebaseAuth.getInstance();
     }
 
 }
